9dab5d1e92f98ea0471369fecc28c8c5dd1ad46f
spences10 <spences10apps@gmail.com>
2022-03-06 18:41:57 +0000

    feat: :sparkles: add typography

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css'
 </script>
 
-<main class="container max-w-2xl mx-auto px-4 mb-20">
+<main class="container max-w-3xl mx-auto px-4 prose prose-xl">
   <slot />
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ const config = {
     extend: {},
   },
 
-  plugins: [require('daisyui')],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
 
 module.exports = config
