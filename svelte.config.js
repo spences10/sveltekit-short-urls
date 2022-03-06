ab32f62d686ee5866b56154fb93be5d3a4031529
spences10 <spences10apps@gmail.com>
2022-03-06 15:58:21 +0000

    chore: :wrench: config short paths for routes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto'
+import path from 'path'
 import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,6 +10,16 @@ const config = {
 
   kit: {
     adapter: adapter(),
+    vite: {
+      resolve: {
+        alias: {
+          $components: path.resolve('./src/lib/components'),
+          $lib: path.resolve('./src/lib'),
+          '$lib/*': path.resolve('./src/lib/*'),
+          $stores: path.resolve('./src/stores'),
+        },
+      },
+    },
   },
 }
 
