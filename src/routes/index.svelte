5a31dea36a3f3f798776065d70cd6c8ea358c90e
spences10 <spences10apps@gmail.com>
2022-06-18 16:37:05 +0100

    refactor: :recycle: redirect to scottspence.com

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,10 @@
-<script lang="ts">
-  import { shortUrls } from '$lib/short-urls'
-  const { redirects } = shortUrls
+<script lang="ts" context="module">
+  export const load = async ({ url: { pathname } }) => {
+    if (pathname === '/') {
+      return {
+        status: 302,
+        redirect: 'https://scottspence.com',
+      }
+    }
+  }
 </script>
-
-<h1>Short URLs with SvelteKit</h1>
-
-<p>
-  This is my short links page, each Source (or <code>pathname</code>)
-  here redirects to destination.
-</p>
-
-<p>
-  So if you want to check out <code>/me</code> then enter the URL for
-  this site with <code>/me</code> at the end of the URL to be redirected.
-</p>
-
-<p>Or, seeing as you're here you can just click the link! 😂</p>
-
-<table class="table">
-  <!-- head -->
-  <thead class="sticky top-0">
-    <tr>
-      <th class="text-lg">Source</th>
-      <th class="text-lg">Destination</th>
-    </tr>
-  </thead>
-  <tbody>
-    <!-- rows -->
-    {#each redirects as redirect}
-      <tr class="hover">
-        <td class="text-xl">
-          <a class='text-secondary' href={redirect.source}>
-            {redirect.source}
-          </a>
-        </td>
-        <td class="text-xl">
-          <a
-            class="link text-primary"
-            href={redirect.destination}
-            target="_blank"
-            rel="noopener"
-          >
-            {redirect.destination}
-          </a>
-        </td>
-      </tr>
-    {/each}
-  </tbody>
-</table>
