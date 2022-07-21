132bf4a83f8db46b96f17eb539a5fb49a8147d30
spences10 <spences10apps@gmail.com>
2022-07-21 22:00:15 +0100

    refactor: :recycle: use env-cmd for env variables

--- src/routes/links/index.json.ts
+++ src/routes/links/index.json.ts
@@ -1,10 +1,9 @@
+import { AIRTABLE_BASE_ID, AIRTABLE_TOKEN } from '$lib/env-vars'
+
 export const GET = async () => {
-  const AIRTABLE_BASE_ID = import.meta.env.VITE_AIRTABLE_BASE_ID
-  const AIRTABLE_TOKEN = import.meta.env.VITE_AIRTABLE_TOKEN
   const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/links-list`
   try {
     const res = await fetch(AIRTABLE_URL, {
-      method: 'GET',
       headers: {
         Authorization: `Bearer ${AIRTABLE_TOKEN}`,
         'Content-Type': 'application/json',
