727e9cddddb35dd203f3a08c4de707e2caa212aa
spences10 <spences10apps@gmail.com>
2022-07-22 13:26:16 +0100

    refactor: :recycle: use Airtable for redirects

--- src/routes/[slug].ts
+++ src/routes/[slug].ts
@@ -1,16 +1,24 @@
-import { shortUrls } from '$lib/short-urls'
+import { AIRTABLE_BASE_ID, AIRTABLE_TOKEN } from '$lib/env-vars'
 import type { RequestHandler } from '@sveltejs/kit'
 
-export const get: RequestHandler = async ({ url }) => {
-  const { redirects } = shortUrls
+export const GET: RequestHandler = async ({ url }) => {
+  const AIRTABLE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/links-list`
+  const res = await fetch(AIRTABLE_URL, {
+    headers: {
+      Authorization: `Bearer ${AIRTABLE_TOKEN}`,
+      'Content-Type': 'application/json',
+      'Cache-Control': 'max-age=0, s-maxage=3600',
+    },
+  })
+  const { records } = await res.json()
 
-  const [redirect] = redirects.filter(
-    item => item.source === url.pathname
+  const [redirect] = records.filter(
+    item => item.fields.source === url.pathname
   )
 
   if (redirect) {
     return {
-      headers: { Location: redirect.destination },
+      headers: { Location: redirect.fields.destination },
       status: 302,
     }
   } else if (!redirect && url.pathname.length > 1) {
