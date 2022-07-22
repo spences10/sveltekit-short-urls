418c6fa80c19524c884e48c51b3422b74730c124
spences10 <spences10apps@gmail.com>
2022-07-22 19:19:25 +0100

    style: :lipstick: remove table, gross on mobile

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css'
 </script>
 
-<main class="container max-w-3xl mx-auto px-4 prose prose-xl">
+<main class="container max-w-3xl mx-auto px-4">
   <slot />
 </main>
--- src/routes/links/index.svelte
+++ src/routes/links/index.svelte
@@ -2,50 +2,48 @@
   export let records: any[] = []
 </script>
 
-<h1>Short URLs with SvelteKit</h1>
+<article class="prose prose-xl mb-10">
+  <h1>Short URLs with SvelteKit</h1>
 
-<p>
-  This is my short links page, each Source (or <code>pathname</code>)
-  here redirects to destination.
-</p>
+  <p>
+    This is my short links page, each Source (or <code>pathname</code
+    >) here redirects to destination.
+  </p>
 
-<p>
-  So if you want to check out <code>/me</code> then enter the URL for
-  this site with <code>/me</code> at the end of the URL to be redirected.
-</p>
+  <p>
+    So if you want to check out <code>/me</code> then enter the URL
+    for this site with <code>/me</code> at the end of the URL to be redirected.
+  </p>
 
-<p>Or, seeing as you're here you can just click the link! 😂</p>
+  <p>Or, seeing as you're here you can just click the link! 😂</p>
+</article>
 
-<table class="table">
-  <!-- head -->
-  <thead class="sticky top-0">
-    <tr>
-      <th class="text-lg">Source</th>
-      <th class="text-lg">Destination</th>
-    </tr>
-  </thead>
-  <tbody>
-    <!-- rows -->
-    {#each records as { fields: { destination, source, visible } }}
-      {#if visible}
-        <tr class="hover">
-          <td class="text-xl">
-            <a class="text-secondary" href={source}>
-              {source}
-            </a>
-          </td>
-          <td class="text-xl">
-            <a
-              class="link text-primary"
-              href={destination}
-              target="_blank"
-              rel="noopener"
-            >
-              {destination}
-            </a>
-          </td>
-        </tr>
-      {/if}
-    {/each}
-  </tbody>
-</table>
+<ul>
+  {#each records as { fields: { description, source, destination, visible } }}
+    {#if visible}
+      <li class="prose prose-xl bg-secondary/10 rounded-md p-4 my-4 border border-primary">
+        <p>
+          Description:
+          {description}
+        </p>
+        <p>
+          Source:
+          <a class="text-secondary" href={source}>
+            {source}
+          </a>
+        </p>
+        <p>
+          Destination:
+          <a
+            class="link text-primary"
+            href={destination}
+            target="_blank"
+            rel="noopener"
+          >
+            {destination}
+          </a>
+        </p>
+      </li>
+    {/if}
+  {/each}
+</ul>
