5cb48a2760b988bb4878df250f8b37079d360b05
spences10 <spences10apps@gmail.com>
2023-05-09 19:37:30 +0100

    test: :white_check_mark: add basic tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test'
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/links')
-	expect(await page.textContent('h1')).toBe(
-		'Short URLs with SvelteKit'
-	)
+	await expect(
+		page.getByRole('heading', { name: 'Short URLs with SvelteKit' })
+	).toBeVisible()
 })
