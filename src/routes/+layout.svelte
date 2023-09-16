8dbc1207140a0b89dad30ff2a9d3bad1173d7b87
spences10 <spences10apps@gmail.com>
2023-09-16 16:57:38 +0100

    feat: :sparkles: add and config fathom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
+	import * as Fathom from 'fathom-client'
+	import { browser } from '$app/environment'
+	import { page } from '$app/stores'
+	import {
+		PUBLIC_FATHOM_ID,
+		PUBLIC_FATHOM_URL,
+	} from '$env/static/public'
+	import { onMount } from 'svelte'
 	import '../app.css'
+
+	onMount(() => {
+		Fathom.load(PUBLIC_FATHOM_ID, {
+			url: PUBLIC_FATHOM_URL,
+			excludedDomains: ['localhost'],
+		})
+	})
+
+	$: $page.url.pathname, browser && Fathom.trackPageview()
 </script>
 
 <main class="container max-w-3xl mx-auto px-4">
