4822c87e7cccc6bcbc539dfdf2aab1b742b0b11d
spences10 <spences10apps@gmail.com>
2024-01-01 08:32:25 +0000

    migrate to SvelteKit 2

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,6 +2,6 @@ import { redirect } from '@sveltejs/kit'
 
 export const load = async ({ url: { pathname } }) => {
 	if (pathname === '/') {
-		throw redirect(302, 'https://scottspence.com')
+		redirect(302, 'https://scottspence.com');
 	}
 }
