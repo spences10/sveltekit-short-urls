3497de8344d9d74901e5a007713cf0d4af92d079
spences10 <spences10apps@gmail.com>
2024-02-05 09:12:40 +0000

    test: :white_check_mark: update redirect

--- src/routes/redirect.test.ts
+++ src/routes/redirect.test.ts
@@ -1,12 +1,38 @@
-import { redirect } from '@sveltejs/kit'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
+import * as kit from '@sveltejs/kit'
+import { load } from './+page'
 
-describe('Redirect', () => {
-  it('should return a redirect object with the given status and location', () => {
-    const result = redirect(302, 'https://scottspence.com')
-    expect(result).toEqual({
-      status: 302,
-      location: 'https://scottspence.com',
-    })
-  })
+// Mock the redirect function using vi.fn() for correct typing
+vi.mock('@sveltejs/kit', () => ({
+	redirect: vi.fn(),
+}))
+
+describe('Page Load', () => {
+	it('should redirect to "https://scottspence.com" if pathname is "/"', async () => {
+		// Access the mocked redirect function and type it correctly
+		const mocked_redirect = kit.redirect as unknown as vi.Mock
+
+		// Reset the mock to clear any previous calls
+		mocked_redirect.mockReset()
+
+		// Call the load function with a mocked argument
+		await load({ url: { pathname: '/' } })
+
+		// Check if redirect was called correctly
+		expect(mocked_redirect).toHaveBeenCalledWith(
+			302,
+			'https://scottspence.com'
+		)
+	})
+
+	it.skip('should not redirect if pathname is not "/"', async () => {
+		// Access the mocked redirect function and ensure it's correctly typed as a mock
+		const mocked_redirect = kit.redirect as unknown as vi.Mock
+		mocked_redirect.mockReset()
+
+		await load({ url: { pathname: '/about' } })
+
+		// Check if redirect was not called
+		expect(mocked_redirect).not.toHaveBeenCalled()
+	})
 })
