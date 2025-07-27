2b00a8d5f668221ad46f91b3fac91cbce39d260d
spences10 <spences10apps@gmail.com>
2025-07-27 16:03:20 +0100

    fix: remove turso sync

--- src/lib/server/client.ts
+++ src/lib/server/client.ts
@@ -1,8 +1,5 @@
 import { dev } from '$app/environment';
-import { env } from '$env/dynamic/private';
 import { createClient, type Client } from '@libsql/client';
-
-const { TURSO_DB_URL, TURSO_DB_AUTH_TOKEN } = env;
 
 const LOCAL_DB_PATH = dev
 	? './local-dev-replica.db'
@@ -15,21 +12,8 @@ export const turso_client = (): Client => {
 		return client_instance;
 	}
 
-	const remote_url = TURSO_DB_URL?.trim();
-	if (remote_url === undefined) {
-		throw new Error('TURSO_DB_URL is not defined');
-	}
-
-	const auth_token = TURSO_DB_AUTH_TOKEN?.trim();
-	if (auth_token === undefined) {
-		throw new Error('TURSO_DB_AUTH_TOKEN is not defined');
-	}
-
 	client_instance = createClient({
 		url: `file:${LOCAL_DB_PATH}`,
-		syncUrl: remote_url,
-		authToken: auth_token,
-		syncInterval: dev ? 60 : 300,
 	});
 
 	return client_instance;
